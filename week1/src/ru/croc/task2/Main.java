039326f7a30979d395293143ea2f1e9d92b24b0b
Saxaar <kuzmiku99@gmail.com>
2021-10-13 21:47:06 +0300

    task2

---
+++ week1/src/ru/croc/task2/Main.java
@@ -0,0 +1,40 @@
+package ru.croc.task2;
+
+import java.util.Scanner;
+
+public class Main {
+    static int i;
+    public static void main(String[] args) {
+        //int i;
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        float b = a;
+        for (i = 0; b > 1024;) {
+            b = b /1024;
+            i++;
+        }
+
+            switch (i) {
+                case 1:
+                    System.out.println(String.format("%.1f",b)+" KB");
+                    break;
+                case 2:
+                    System.out.println(String.format("%.1f",b)+" MB");
+                    break;
+                case 3:
+                    System.out.println(String.format("%.1f",b)+" GB");
+                    break;
+                case 4:
+                    System.out.println(String.format("%.1f",b)+" TB");
+                    break;
+                case 5:
+                    System.out.println(String.format("%.1f",b)+" PB");
+                    break;
+                default:
+                    System.out.println(b + "B");
+            }
+
+
+    }
+
+}
